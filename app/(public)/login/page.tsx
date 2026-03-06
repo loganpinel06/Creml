b0e8fc3300f87c47c3fe78fa6bc16cc4ae3220db
Logan Pinel <loganpinel@outlook.com>
2026-03-05 21:49:02 -0500

    Supabase setup, working on prisma setup now

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -13,7 +13,11 @@ export default function LoginPage() {
     <div className="min-h-screen bg-gray-50 py-12">
       <Header />
       <div className="max-w-md mx-auto px-4 sm:px-6 lg:px-8 mt-15">
-        {message && <p>{message}</p>}
+        {message && (
+          <div className="mb-4 p-4 bg-blue-50 border border-blue-200 rounded-lg">
+            <p className="text-blue-800 text-sm text-center">{message}</p>
+          </div>
+        )}
         <SignInForm />
       </div>
     </div>
--- utils/requireAuth.ts
+++ utils/requireAuth.ts
@@ -3,17 +3,21 @@ import { NextResponse } from "next/server";
 import { SupabaseClient } from "@supabase/supabase-js"; //for typing
 
 export async function requireAuth(supabase: SupabaseClient, request: Request) {
-    //check if there is a current user logged in
-    const {data: { user }, error } = await supabase.auth.getUser();
-    //return an error response if there is no user or if there was an error fetching the user
-    if (!user || error) {
-        return {
-            user: null,
-            //send a response that redirects users to sign in page
-            response: NextResponse.redirect(new URL("/signin?message=Please+sign+in+to+continue", request.url)),
-        }
-    }
-    //return the user to the route.ts file if valid
-    return { user, response: null };
+  //check if there is a current user logged in
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  //return an error response if there is no user or if there was an error fetching the user
+  if (!user || error) {
+    return {
+      user: null,
+      //send a response that redirects users to sign in page
+      response: NextResponse.redirect(
+        new URL("/login?message=Please+sign+in+to+continue", request.url),
+      ),
+    };
+  }
+  //return the user to the route.ts file if valid
+  return { user, response: null };
 }
-
